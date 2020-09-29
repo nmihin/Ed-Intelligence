71c2a64226eb317f3d08b4578b184981e6bb8fb1
nmihin <nmihin@gmail.com>
2020-09-29 16:42:36 +0200

    - added calendar section and styling

---
+++ js/script.js
@@ -0,0 +1,10 @@
+(function() {
+  // Material datepicker
+  const datepicker = function() {
+    // Standard Bar Chart
+    $('#datepicker-inline').datetimepicker({
+      inline: true
+    });
+  };
+  window.onload = datepicker;
+})();
