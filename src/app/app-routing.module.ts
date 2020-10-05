d560831ee346be53be4ef73e4ebe93d04fce67ce
nmihin <nmihin@gmail.com>
2020-10-05 14:20:28 +0200

    - angular added refactoring

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AdminComponent } from './admin/admin.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  { path: '', component: AdminComponent },
+  { path: 'admin', component: AdminComponent },
+  { path: 'home', component: HomeComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
