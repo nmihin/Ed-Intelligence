d32e71b583627ca2b1d380407a41085026b7f52f
nmihin <nmihin@gmail.com>
2020-10-12 19:46:10 +0200

    - removed redundant libraries

--- src/main.js
+++ src/main.js
@@ -4,20 +4,10 @@ import App from './App.vue';
 import router from './router';
 import $ from 'jquery';
 import VueMoment from 'vue-moment'
-// import style (>= Swiper 6.x)
-import 'swiper/swiper-bundle.css'
 
 import './assets/css/styles.css';
-//import './assets/css/bootstrap-datetimepicker.min.css';
-//import './assets/css/bootstrap-glyphicons.css';
 import './assets/css/bootstrap-grid.min.css';
 import './assets/css/bootstrap.min.css';
-//import './assets/css/pmd-datetimepicker.css';
-import './assets/css/swiper.min.css';
-
-//import './assets/js/bootstrap-datetimepicker.min.js';
-import './assets/js/swiper.min.js';
-
 
 Vue.config.productionTip = false;
 
