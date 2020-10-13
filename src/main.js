486cd7a3813d670480d66eb38aecbf8fcfb8565d
nmihin <nmihin@gmail.com>
2020-10-13 06:37:12 +0200

    - test deploy

--- src/main.js
+++ src/main.js
@@ -1,18 +1,8 @@
 import Vue from 'vue';
 import App from './App.vue';
-//import VueRouter from "vue-router";
-import router from './router';
-import $ from 'jquery';
-
-import './assets/css/styles.css';
-import './assets/css/swiper.min.css';
-import './assets/css/bootstrap-grid.min.css';
-import './assets/css/bootstrap.min.css';
 
 Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-  router,
-  $
+  render: h => h(App)
 }).$mount("#app");
